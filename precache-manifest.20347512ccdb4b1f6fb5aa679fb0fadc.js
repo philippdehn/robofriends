70c34db5df92cd3e8ca9805e3e5782383f04feea
Philipp Dehn <philippdehn@icloud.com>
2020-05-04 20:04:09 +0200

    Updates

---
+++ precache-manifest.20347512ccdb4b1f6fb5aa679fb0fadc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8a5c5b09654be0951b42838518679d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4963fd8488d98bc12e16",
+    "url": "/robofriends/static/css/2.a489a27e.chunk.css"
+  },
+  {
+    "revision": "8e08d17585eb9d0d3df0",
+    "url": "/robofriends/static/css/main.59929819.chunk.css"
+  },
+  {
+    "revision": "4963fd8488d98bc12e16",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e08d17585eb9d0d3df0",
+    "url": "/robofriends/static/js/main.37cb0dde.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
